38162d7a28e656c29df797b7eb5bb743c12eeeca
Robin <robinlievens24@gmail.com>
2020-03-04 14:37:57 +0100

    Fixed script

--- verbal_memory.py
+++ verbal_memory.py
@@ -2,30 +2,30 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 import time
 
-AMOUNT_POINTS = 420
+AMOUNT_POINTS = 169
 
 def main():
     wordlist = []
-    driver = webdriver.Chrome()
+    driver = webdriver.Firefox()
     driver.implicitly_wait(10)
     driver.get('https://www.humanbenchmark.com/tests/verbal-memory')
     print('Log in now and return to the verbal memory test page. Press ENTER to continue')
     input()
 
-    start_button = driver.find_element_by_css_selector('.button.start')
+    start_button = driver.find_element_by_css_selector('.hero-button')
     start_button.click()
     for i in range(AMOUNT_POINTS):
         time.sleep(0.01)
-        word = driver.find_element_by_css_selector('.word.ng-scope').text
+        word = driver.find_element_by_css_selector('.word').text
         if word not in wordlist:
             wordlist.append(word)
             print(f'{i+1}: NEW {word}')
-            driver.find_element_by_link_text('NEW').click()
+            driver.find_element_by_xpath('/html/body/div/div/div[4]/div[1]/div/div/div[3]/button[2]').click()
         else:
             print(f'{i+1}: {word}')
-            driver.find_element_by_link_text('SEEN').click()
+            driver.find_element_by_xpath('/html/body/div/div/div[4]/div[1]/div/div/div[3]/button[1]').click()
     print(f'Total amount of words remembered: {len(wordlist)}')
 
 if __name__ == "__main__":
     main()
-    input('')
+    input('Press any key to exit')
